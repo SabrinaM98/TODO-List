f4586c71737f6b332cbd33ac74ecd5ff968ad8aa
SabrinaM98 <sabrinarolim98@gmail.com>
2025-08-27 19:19:14 -0300

    feat: model for user entity

---
+++ src/main/java/br/com/myjava/todolist/user/UserModel.java
@@ -0,0 +1,36 @@
+package br.com.myjava.todolist.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
+    public UserModel() {}
+
+    public UserModel(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+}
