5771afe2d9e76936435d107ffb215de4b144b0ac
SabrinaM98 <sabrinarolim98@gmail.com>
2025-09-06 16:58:47 -0300

    feat: set userId from basicAuth

--- src/main/java/br/com/myjava/todolist/task/TaskController.java
+++ src/main/java/br/com/myjava/todolist/task/TaskController.java
@@ -1,11 +1,15 @@
 package br.com.myjava.todolist.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,9 +19,12 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         TaskModel task = null;
         try {
+            taskModel.setUserId((UUID) request.getAttribute("userId"));
+            var currentTime = LocalDateTime.now();
+            taskModel.setCreateDate(currentTime);
             task = this.taskRepository.save(taskModel);
         } catch (Exception e) {
             ResponseEntity.badRequest().body(e.getMessage());
