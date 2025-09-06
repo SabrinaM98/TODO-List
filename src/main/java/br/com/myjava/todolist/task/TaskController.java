1151781ca27f0b04757c76873748e224f4497e5d
SabrinaM98 <sabrinarolim98@gmail.com>
2025-09-06 16:36:39 -0300

    feat: endpoint to create tasks

---
+++ src/main/java/br/com/myjava/todolist/task/TaskController.java
@@ -0,0 +1,27 @@
+package br.com.myjava.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+        TaskModel task = null;
+        try {
+            task = this.taskRepository.save(taskModel);
+        } catch (Exception e) {
+            ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.status(201).body(task);
+    }
+}
---
+++ src/main/java/br/com/myjava/todolist/task/TaskModel.java
@@ -0,0 +1,42 @@
+package br.com.myjava.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(length = 50)
+    private String title;
+    private String description;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    @CreationTimestamp
+    private LocalDateTime createDate;
+    private UUID userId;
+
+    public TaskModel() {}
+
+    public TaskModel(UUID id, String title, String description, LocalDateTime startAt, LocalDateTime endAt, String priority, LocalDateTime createDate, UUID userId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.priority = priority;
+        this.createDate = createDate;
+        this.userId = userId;
+    }
+
+}
